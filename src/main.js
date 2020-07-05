fc59dd0c6d7e7a94d1e4be3a4142206492cb9d8b
yuhan <40864785+wbyuhan@users.noreply.github.com>
2020-07-06 01:10:16 +0800

    feat:add  in Vue.prototype

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import * as joint from 'jointjs'
+Vue.prototype.$joint = joint
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,60 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div id="wrapper" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+
+  },
+  mounted() {
+    this.init()
+  },
+  methods: {
+    init() {
+      const graph = new this.$joint.dia.Graph()
+      new this.$joint.dia.Paper({
+        el: document.getElementById('wrapper'),
+        width: 900,
+        height: 700,
+        model: graph,
+        gridSize: 1,
+        drawGrid: true,
+        background: {
+          color: 'rgba(0, 255, 0, 0.3)'
+        }
+      })
+
+      const rect = new this.$joint.shapes.basic.Rect({
+        position: { x: 100, y: 30 },
+        size: { width: 100, height: 30 },
+        attrs: {
+          rect: {
+            fill: 'blue'
+          },
+          text: {
+            text: 'first Box',
+            fill: 'white'
+          }
+        }
+      })
+      console.log('%c 🍮 rect: ', 'font-size:20px;background-color: #33A5FF;color:#fff;', rect)
+      rect.addTo(graph)
+      var rect2 = rect.clone()
+      rect2.translate(300, 0)
+      rect2.attr('label/text', 'World!')
+      rect2.addTo(graph)
+
+      var link = new this.$joint.shapes.standard.Link({
+        source: { id: rect.id },
+        target: { id: rect2.id }
+      })
+      link.addTo(graph)
+    }
   }
 }
 </script>
